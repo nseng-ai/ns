3888d9ebf2f2dee38abb607981f247a1a6f30b79
Nick Schrock <schrockn@dagsterlabs.com>
2026-04-12 16:36:05 -0400

    pi-extension

---
+++ .pi/extensions/twerk-objectives.ts
@@ -0,0 +1,9 @@
+import type { ExtensionAPI } from "@mariozechner/pi-coding-agent";
+
+/**
+ * Deprecated shim file.
+ *
+ * The objectives extension now lives at:
+ *   .pi/extensions/objective-list.ts
+ */
+export default function deprecatedTwerkObjectivesExtension(_pi: ExtensionAPI) {}
