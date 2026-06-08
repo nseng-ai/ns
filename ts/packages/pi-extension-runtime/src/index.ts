ae49f61c91c1d07b9582d5da7a08e9caf31bab1f
Nick Schrock <schrockn@dagsterlabs.com>
2026-06-08 05:44:53 -0400

    Address PR review comments (batch 5/5)
    - Share branch slug normalization from runtime

--- ts/packages/pi-extension-runtime/src/index.ts
+++ ts/packages/pi-extension-runtime/src/index.ts
@@ -1,6 +1,7 @@
 export {
 	finalizeBranchSlug,
 	MAX_BRANCH_SLUG_LENGTH,
+	normalizeBranchSlugText,
 	sanitizeBranchName,
 	trimBranchSlugToLength,
 } from "./branch-slug.ts";
--- ts/packages/pi-extensions/src/branch-slug.ts
+++ ts/packages/pi-extensions/src/branch-slug.ts
@@ -1,13 +1,7 @@
-import { finalizeBranchSlug } from "@asdl/pi-extension-runtime/branch-slug";
-
-export { finalizeBranchSlug, MAX_BRANCH_SLUG_LENGTH, sanitizeBranchName, trimBranchSlugToLength } from "@asdl/pi-extension-runtime/branch-slug";
-
-export function normalizeBranchSlugText(value: string): string {
-	return value
-		.toLowerCase()
-		.normalize("NFKD")
-		.replace(/[\u0300-\u036f]/g, "")
-		.replace(/[^a-z0-9]+/g, "-")
-		.replace(/-+/g, "-")
-		.replace(/^-|-$/g, "");
-}
+export {
+	finalizeBranchSlug,
+	MAX_BRANCH_SLUG_LENGTH,
+	normalizeBranchSlugText,
+	sanitizeBranchName,
+	trimBranchSlugToLength,
+} from "@asdl/pi-extension-runtime/branch-slug";
