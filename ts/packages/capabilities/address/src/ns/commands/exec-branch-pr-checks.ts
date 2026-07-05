24188e19d05324de341a48da09543a103f5afee2
Nick Schrock <schrockn@dagsterlabs.com>
2026-07-04 19:53:29 -0700

    [cp] Use ns extension for branch PR checks
    - Swap `prAddressSdlExtension` for `prAddressNsExtension`
    - Update `branch-pr-checks` command wiring accordingly

--- ts/packages/capabilities/address/src/ns/commands/exec-branch-pr-checks.ts
+++ ts/packages/capabilities/address/src/ns/commands/exec-branch-pr-checks.ts
@@ -1,3 +1,3 @@
-import { prAddressSdlExtension } from "../../ns-command.ts";
+import { prAddressNsExtension } from "../../ns-command.ts";
 
-export default prAddressSdlExtension("branch-pr-checks");
+export default prAddressNsExtension("branch-pr-checks");
