00a63c6e10913c4fde9f9a66bd960b6f74c3c107
schrockn <schrockn@vercel.com>
2026-07-26 13:02:46 -0700

    [cp] Remove obsolete guard test scripts
    - Delete test-guard.js
    - Delete test-guard.ts

--- test-guard.js
+++
@@ -1,5 +0,0 @@
-import { commandTargetsHomeRoot, tokenizeShellLikeCommand, pathValueTargetsHomeRoot } from "./.pi/extensions/home-directory-guard.js";
-
-console.log(commandTargetsHomeRoot("ls /Users/schrockn/"));
-console.log(tokenizeShellLikeCommand("ls /Users/schrockn/"));
-console.log(pathValueTargetsHomeRoot("/Users/schrockn/"));
