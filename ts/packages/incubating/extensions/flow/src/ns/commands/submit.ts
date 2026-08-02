09c1e1d83e8597b6d2d3eacfbd2024e1ff88553a
schrockn <schrockn@vercel.com>
2026-08-02 15:21:30 -0700

    Use optionalEntry for title prefix forwarding

--- ts/packages/incubating/extensions/flow/src/ns/commands/submit.ts
+++ ts/packages/incubating/extensions/flow/src/ns/commands/submit.ts
@@ -187,7 +187,7 @@ export function createFlowSubmitCommand(
 			return await runSubmitWithProgress({
 				ctx,
 				request,
-				...(titlePrefix === undefined ? {} : { titlePrefix: titlePrefix.prefix }),
+				...optionalEntry("titlePrefix", titlePrefix?.prefix),
 				runtime,
 				checksLoad,
 				checkpointContext,
